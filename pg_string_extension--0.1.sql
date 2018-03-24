29c2e20231c4ae0f651cd1750978ece0d17a39ac
Joseph Mckulka <jmckulk@Josephs-MacBook-Pro.local>
2018-03-24 12:51:17 -0400

    adding sort_chars, is_anagram, and helper functions

--- pg_string_extension--0.1.sql
+++ pg_string_extension--0.1.sql
@@ -8,3 +8,11 @@ CREATE FUNCTION echo_text(text) RETURNS text
 CREATE FUNCTION is_palindrome(text) RETURNS boolean
   AS '$libdir/pg_string_extension', 'is_palindrome'
   LANGUAGE C STRICT;
+
+CREATE FUNCTION sort_chars(text) RETURNS text
+  AS '$libdir/pg_string_extension', 'sort_chars'
+  LANGUAGE C STRICT;
+
+CREATE FUNCTION is_anagram(text, text) RETURNS boolean
+  AS '$libdir/pg_string_extension', 'is_anagram'
+  LANGUAGE C STRICT;
--- pg_string_extension.c
+++ pg_string_extension.c
@@ -10,6 +10,22 @@ PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(echo_text);
 
+int compare(const void*, const void*);
+int len_with_no_spaces(const char *s);
+int len_with_no_spaces(const char *s){
+  const char * sc;
+
+  sc = s;
+
+  while(*sc != '\0'){
+    if(*sc != ' '){
+      sc++;
+    }
+  }
+
+  return sc - s;
+}
+
 Datum
 echo_text(PG_FUNCTION_ARGS){
   text *t = PG_GETARG_TEXT_PP(0);
@@ -49,6 +65,67 @@ is_palindrome(PG_FUNCTION_ARGS){
       back--;
     }
   }
+
+  PG_RETURN_BOOL(true);
+}
+
+int compare(const void* a, const void* b){
+  char char_a = *((char*) a);
+  char char_b = *((char*) b);
+
+  if(char_a == char_b){
+    return 0;
+  } else if(char_a < char_b){
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+PG_FUNCTION_INFO_V1(sort_chars);
+
+Datum
+sort_chars(PG_FUNCTION_ARGS){
+  text *unsorted = PG_GETARG_TEXT_PP(0);
+  char *unsorted_c = text_to_cstring(unsorted);
+  int len = strlen(unsorted_c);
+
+  qsort(unsorted_c, len, sizeof(char), compare);
+
+  text *sorted = cstring_to_text(unsorted_c);
+
+  PG_RETURN_TEXT_P(sorted);
+}
+
+PG_FUNCTION_INFO_V1(is_anagram);
+
+Datum
+is_anagram(PG_FUNCTION_ARGS){
+  text *word_a = PG_GETARG_TEXT_PP(0);
+  text *word_b = PG_GETARG_TEXT_PP(1);
+
+  char *c_word_a = text_to_cstring(word_a);
+  char *c_word_b = text_to_cstring(word_b);
+
+  int len_a = len_with_no_spaces(c_word_a);
+  int len_b = len_with_no_spaces(c_word_b);
+
+  if(len_a != len_b){
+    PG_RETURN_BOOL(false);
+  }
+
+  qsort(c_word_a, strlen(c_word_a), sizeof(char), compare);
+  qsort(c_word_b, strlen(c_word_b), sizeof(char), compare);
+
+  int len = strlen(c_word_a);
+  int i;
+
+  for(i = 0; i < len; i++){
+    if(c_word_a[i] != c_word_b[i]){
+      PG_RETURN_BOOL(false);
+    }
+  }
+
 
   PG_RETURN_BOOL(true);
 }
